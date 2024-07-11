d5c85c25e6c2173db17227509fd3e7cd6c8b90de
chensy <chensy.cao@foxmail.com>
2024-07-11 15:12:33 +0800

    20240606: update spark.

--- kivi/spark/__init__.py
+++ kivi/spark/__init__.py
@@ -1,5 +1,6 @@
 from .column import *
 from .dataframe import *
+from .operator import *
 from .partition import *
 from .session import *
 from .table import *
---
+++ kivi/spark/operator.py
@@ -0,0 +1,38 @@
+try:
+    from pyspark.sql.window import Window
+    from pyspark.sql import functions as F
+    from pyspark.sql import DataFrame as TypeSparkDataFrame
+except ImportError:
+    print("pyspark not installed, please install it first. < pip install pyspark >")
+from typing import List, Optional
+from pandas import DataFrame as TypePandasDataFrame
+
+
+__all__ = [
+    "pct_rank",
+]
+
+
+def pct_rank(
+        df: TypePandasDataFrame,
+        column_name,
+        index_name: str = None,
+        partition_by: Optional[List[str]] = None,
+) -> TypePandasDataFrame:
+    """
+    描述：返回依据行业作为窗口的行业排名 DataFrame
+    :param df: 原始数据
+    :param column_name: 新指标名称
+    :param index_name: 原指标名称
+    :param partition_by: 窗口字段
+    :return: spark DataFrame
+    """
+    if partition_by is None:
+        partition_by = ['load_date', 'industry']
+
+    if index_name is None:
+        index_name = column_name
+    # window of industry rank
+    wind_indu = Window.partitionBy(*partition_by).orderBy(F.desc(column_name))
+    df = df.withColumn(f'rank_indu_{index_name}', F.percent_rank().over(wind_indu))
+    return df
