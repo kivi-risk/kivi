5271b837904127617089b1e3218dddd24bd4d163
chensy <chensy.cao@foxmail.com>
2024-07-23 12:25:18 +0800

    20240606: remove

--- kivi/DesStatistic/__init__.py
+++
@@ -1,6 +0,0 @@
-from .DefaultAnalysis import DefaultAnalysis, plot_bin
-from .Descriptive import Des
-from .CategoryData import AnalysisCategory
-from kivi.kivi.kivi import iv
-import warnings
-warnings.filterwarnings("ignore")
