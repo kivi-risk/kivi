cdae060a5e136aee747ea42ed00665c13ee5c026
chensy <chensy.cao@foxmail.com>
2024-07-25 10:47:20 +0800

    20240606: update dataset

--- kivi/datasets/dataset.py
+++ kivi/datasets/dataset.py
@@ -1,14 +1,16 @@
 import os
+import numpy as np
 import pandas as pd
-
-
-path = os.path.dirname(__file__)
+from typing import Optional
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
 
 
 __all__ = [
     "Dataset",
     "MakeData",
 ]
+path = os.path.dirname(__file__)
 
 
 class Dataset:
@@ -43,17 +45,32 @@ class Dataset:
 
 class MakeData:
 
-    def __init__(self):
-        pass
+    def __init__(
+            self,
+            n_samples: Optional[int] = 10000,
+            n_features: Optional[int] = 5,
+            n_informative: Optional[int] = 5,
+            n_redundant: Optional[int] = 0,
+            test_size: Optional[float] = 0.3,
+    ):
+        self.n_samples = n_samples
+        self.n_features = n_features
+        self.n_informative = n_informative
+        self.n_redundant = n_redundant
+        self.test_size = test_size
 
     def dataset(self, ):
-        from sklearn import datasets
-        import numpy as np
         np.random.seed(0)
         x, y = datasets.make_classification(
-            n_samples=100000, n_features=20,
-            n_informative=2, n_redundant=2)
-        train_samples = 100
-        x_train, x_test = x[:train_samples], x[train_samples:]
-        y_train, y_test = y[:train_samples], y[train_samples:]
+            n_samples=self.n_samples, n_features=self.n_features,
+            n_informative=self.n_informative, n_redundant=self.n_redundant)
+        x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=self.test_size, random_state=42)
         return x_train, x_test, y_train, y_test
+
+    def sample(self):
+        """"""
+        columns_name = [f"col_{i}" for i in range(self.n_features)]
+        x_train, x_test, y_train, y_test = self.dataset()
+        train = pd.concat([pd.DataFrame(x_train, columns=columns_name), pd.DataFrame(y_train, columns=['target'])], axis=1)
+        test = pd.concat([pd.DataFrame(x_test, columns=columns_name), pd.DataFrame(y_test, columns=['target'])], axis=1)
+        return train, test
